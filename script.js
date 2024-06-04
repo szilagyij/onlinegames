99e24c2586bc4b0b7d31452081cbb63bddfd51d1
SzilagyiJ <SzilagyiJ@kkszki.hu>
2024-06-04 10:51:05 +0200

    main

--- script.js
+++ script.js
@@ -209,3 +209,390 @@ function Amoba(){
 
 
 }
+
+
+
+function Uno(){
+
+    document.getElementById(`container`).innerHTML=""
+    document.getElementById(`container`).innerHTML+=`
+    <h1 id="jelenlegi"></h1>
+    <div class="mainboard" id="mainboard">
+            
+            <div class="insideboard" id="insideboard">
+                <img src="table.png" alt="" class="asztal">
+                <div class="pickup" id="pickup">
+                    <img src="pickupcard.png" alt="pickupcard" class="img-fluid pickupcard">
+                </div>
+                <div class="current" id="current">
+                    
+                </div>
+            </div>
+            <div class="popup" id="popup">
+                <p>Melyik legyen?</p>
+                <div class="red color-box" id="red" style="background-color: red;"></div>
+                <div class="green color-box" id="green" style="background-color: green;"></div>
+                <div class="blue color-box" id="blue" style="background-color: blue;"></div>
+                <div class="yellow color-box" id="yellow" style="background-color: yellow;"></div>
+            </div>
+            <div class="player1" id="player1">
+    
+            </div>
+    
+            <div class="player2" id="player2">
+    
+            </div>
+    
+            <div class="player3" id="player3">
+    
+            </div>
+    
+            <div class="player4" id="player4">
+    
+            </div>
+    
+        </div>
+    `
+    const unoKartyak = [
+        "b0", "b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8", "b9",
+        "r0", "r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8", "r9",
+        "g0", "g1", "g2", "g3", "g4", "g5", "g6", "g7", "g8", "g9",
+        "y0", "y1", "y2", "y3", "y4", "y5", "y6", "y7", "y8", "y9",
+        "bs", "rs", "gs", "ys", // Színváltó kártyák 
+        "bp", "rp", "gp", "yp", // Kihagyó kártyák
+        "bd", "rd", "gd", "yd", // Kettős húzás kártyák
+        "bf", "rf", "gf", "yf",// Négyszeres húzás kártyák
+        "br", "rr", "gr", "yr" //reverse, visszafordító
+    ];
+    
+    
+    let player1cards = [];
+    let player2cards = [];
+    let player3cards = [];
+    let player4cards = [];
+    let currentmove="player1"
+    let nextmove="player2"
+    let pluscard=false;
+    let selectedcolor="none";
+    let alreadyblocked=false;
+    let reversed=false;
+    
+    let jelenlegi =document.getElementById(`jelenlegi`);
+    document.getElementById('pickup').addEventListener('click', function() {
+        let rnd = Math.floor(Math.random() * unoKartyak.length);
+        let currentCards = eval(`${currentmove}cards`);
+        currentCards.push(unoKartyak[rnd]);
+        showcard(); 
+    });
+    
+    function winner(gamewinner){
+        alert(`A játék győztese:${gamewinner}`)
+    }
+    
+    function filltombs() {
+        randomcardgen(player1cards);
+        randomcardgen(player2cards);
+        randomcardgen(player3cards);
+        randomcardgen(player4cards);
+    }
+    
+    
+    
+        function randomcardgen(name) {
+            for (let i = 0; i < 7; i++) {
+                let rnd = Math.floor(Math.random() * unoKartyak.length);
+                name.push(unoKartyak[rnd]);
+            }
+        }
+        
+        function starter(){
+            let startercard=Math.floor(Math.random() * unoKartyak.length);
+            document.getElementById('current').innerHTML = `<img id="currentCard" src="cards/${unoKartyak[startercard]}.png" alt="pickupcard" class="img-fluid pickupcard">`;
+        }
+    
+        function openPopup() {
+            document.getElementById("popup").style.visibility = "visible";
+        }
+        function closePopup() {
+            document.getElementById("popup").style.visibility = "hidden";
+        }
+        
+        
+    
+        document.getElementById(`popup`).addEventListener('click', function(event) {
+            selectedcolor="none"
+            if (event.target.classList.contains('color-box')) {
+                selectedcolor = event.target.style.backgroundColor;
+                closePopup();
+            }
+        });
+    
+    const container=document.getElementById(`player1`)
+    
+    function mindenegben(event,i,player,nextplayer,lastplayer){
+        let currentCard = document.getElementById('currentCard');
+        let clickedCardUrl = event.target.src;
+      
+                if (currentCard.src.split('/')[4][1]=='p' &! alreadyblocked) {
+                    alreadyblocked=true
+                    currentmove=nextplayer
+                    alert("Blokkolva")
+                    showcard()
+                }
+                else{
+                    alreadyblocked=false
+                    showcard()
+                }
+    
+                if (clickedCardUrl.split('/')[4][1]=='r') 
+                {
+                    if (reversed) {
+                        reversed=false
+                    }
+                    else{
+                        reversed=true
+                    }
+                }
+    
+        
+                if (clickedCardUrl.split('/')[4][1]=='s' || clickedCardUrl.split('/')[4][1]=='f') {
+                    openPopup();
+                }
+        if (selectedcolor=="none" &! alreadyblocked) {       //nincs szín kérve, nemvagyunk blokkolva
+    
+                    if (clickedCardUrl.split('/')[4][1]=='r') 
+                    {
+                        if (reversed) {
+                            currentmove=lastplayer
+                        }
+                        else{
+                            currentmove=nextmove
+                        }
+                        if (reversed) {
+                            let indexToRemove = player.indexOf(player[i]);
+                            if (indexToRemove !== -1) {
+                                    player.splice(indexToRemove, 1);      
+                                    console.log(`A kártyát eltávolítottuk a ${currentmove} kártyáiból.`);;
+                                    document.getElementById(`currentCard`).src = clickedCardUrl; 
+                                    if (player.length==0) {
+                                        winner(currentmove)
+                                        location.reload();
+                                    }
+                                    else{
+                                        currentmove=lastplayer         
+                                        showcard();
+                                    }
+                                }
+                        }    
+                        else{                 
+                            let indexToRemove = player.indexOf(player[i]);
+                            if (indexToRemove !== -1) {
+                                    player.splice(indexToRemove, 1);      
+                                    console.log(`A kártyát eltávolítottuk a ${currentmove} kártyáiból.`);
+                                    document.getElementById(`currentCard`).src = clickedCardUrl; 
+                                    if (player.length==0) {
+                                        winner(currentmove)
+                                        location.reload();
+                                    }
+                                    else{
+                                        currentmove=nextmove
+                                        showcard();
+                                    }
+                                }
+                            }
+                        
+                    }
+                    else{
+                        if (reversed) {
+                            if (currentCard.src.split('/')[4][0] === clickedCardUrl.split('/')[4][0] || currentCard.src.split('/')[4][1] === clickedCardUrl.split('/')[4][1]|| clickedCardUrl.split('/')[4][1]=='r' ||  clickedCardUrl.split('/')[4][1]=='d' || clickedCardUrl.split('/')[4][1]=='f' || clickedCardUrl.split('/')[4][1]=='p' || clickedCardUrl.split('/')[4][1]=='s') {
+                                let indexToRemove = player.indexOf(player[i]);
+                                if (indexToRemove !== -1) {
+                                        player.splice(indexToRemove, 1);
+                                        if (clickedCardUrl.split('/')[4][1]=='f' || clickedCardUrl.split('/')[4][1]=='d') {
+                                            pluscard=true
+                                        }
+                                        console.log(`A kártyát eltávolítottuk a ${currentmove} kártyáiból.`);
+                                        document.getElementById(`currentCard`).src = clickedCardUrl; 
+                                        if (player.length==0) {
+                                            winner(currentmove)
+                                            location.reload();
+                                        }
+                                        else{
+                                            currentmove=lastplayer
+                                            showcard();
+                                        }
+                                    }             
+                                } 
+                            else {
+                                alert("A kártya nem lerakható")
+                            }
+                        }
+                        else
+                        {
+                            if (currentCard.src.split('/')[4][0] === clickedCardUrl.split('/')[4][0] || currentCard.src.split('/')[4][1] === clickedCardUrl.split('/')[4][1]|| clickedCardUrl.split('/')[4][1]=='r' ||  clickedCardUrl.split('/')[4][1]=='d' || clickedCardUrl.split('/')[4][1]=='f' || clickedCardUrl.split('/')[4][1]=='p' || clickedCardUrl.split('/')[4][1]=='s') {
+                                let indexToRemove = player.indexOf(player[i]);
+                                if (indexToRemove !== -1) {
+                                        player.splice(indexToRemove, 1);
+                                        if (clickedCardUrl.split('/')[4][1]=='f' || clickedCardUrl.split('/')[4][1]=='d') {
+                                            pluscard=true
+                                        }
+                                        console.log(`A kártyát eltávolítottuk a ${currentmove} kártyáiból.`);
+                                        document.getElementById(`currentCard`).src = clickedCardUrl; 
+                                        if (player.length==0) {
+                                            winner(currentmove)
+                                            location.reload();
+                                        }
+                                        else{
+                                            currentmove=nextplayer
+                                            showcard();
+                                        }
+                                    }             
+                                } 
+                            else {
+                                alert("A kártya nem lerakható")
+                            }
+                        }
+    
+    
+                    } 
+                }
+        
+        else{
+            if (reversed) {
+                if (clickedCardUrl.split('/')[4][0]==selectedcolor[0]) {
+                    let indexToRemove = player.indexOf(player[i]);
+                    if (indexToRemove !== -1) {
+                        player.splice(indexToRemove, 1);
+                        console.log(`A kártyát eltávolítottuk a ${currentmove} kártyáiból.`);
+                        selectedcolor="none"
+                        document.getElementById(`currentCard`).src = clickedCardUrl; 
+                        if (player.length==0) {
+                            winner(currentmove)
+                            location.reload();
+                        }
+                        else{
+                            currentmove=lastplayer
+                            showcard();
+                        }
+                    }
+                }
+            }
+            else{
+                if (clickedCardUrl.split('/')[4][0]==selectedcolor[0]) {
+                    let indexToRemove = player.indexOf(player[i]);
+                    if (indexToRemove !== -1) {
+                        player.splice(indexToRemove, 1);
+                        console.log(`A kártyát eltávolítottuk a ${currentmove} kártyáiból.`);
+                        selectedcolor="none"
+                        document.getElementById(`currentCard`).src = clickedCardUrl; 
+                        if (player.length==0) {
+                            winner(currentmove)
+                            location.reload();
+                        }
+                        else{
+                            currentmove=nextplayer
+                            showcard();
+                        }
+                    }
+                }
+            }
+            
+        }
+    
+    
+    
+        if (pluscard &! alreadyblocked) { 
+            if (currentCard.src.split('/')[4][1]=='d') {
+                for (let i = 0; i < 2; i++) {
+                    let rnd = Math.floor(Math.random() * unoKartyak.length);
+                    player.push(unoKartyak[rnd]);
+                    pluscard=false
+                }    
+            }
+            else if (currentCard.src.split('/')[4][1]=='f') {
+                for (let i = 0; i < 4; i++) {
+                    let rnd = Math.floor(Math.random() * unoKartyak.length);
+                    player.push(unoKartyak[rnd]);
+                    pluscard=false
+                }
+            }
+        }
+    
+            
+    
+    }
+    
+    function showcard(){
+        switch (currentmove) {
+        case "player1":
+            jelenlegi.innerHTML = "Jelenlegi játékos: Player 1";
+            container.innerHTML = '';
+            for (let i = 0; i < player1cards.length; i++) {
+                const img=document.createElement("img");
+                img.src=`cards/${player1cards[i]}.png`;
+                img.classList.add("card")
+                container.appendChild(img)
+                img.addEventListener('click', (event) => {
+                    mindenegben(event,i,player1cards,"player2","player4")
+                });
+            }
+            break;
+        case "player2":
+    
+            jelenlegi.innerHTML = "Jelenlegi játékos: Player 2";
+            container.innerHTML = '';
+            container.innerHTML = '';
+    
+            for (let i = 0; i < player2cards.length; i++) {
+                const img=document.createElement("img");
+                img.src=`cards/${player2cards[i]}.png`;
+                img.classList.add("card")
+                container.appendChild(img)
+                img.addEventListener('click', (event) => {
+                    mindenegben(event,i,player2cards,"player3","player1")
+                });
+            }
+            break;
+        case "player3":
+            jelenlegi.innerHTML = "Jelenlegi játékos: Player 3";
+            container.innerHTML = '';
+            for (let i = 0; i < player3cards.length; i++) {
+                const img=document.createElement("img");
+                img.src=`cards/${player3cards[i]}.png`;
+                img.classList.add("card")
+                container.appendChild(img)
+                img.addEventListener('click', (event) => {
+                    mindenegben(event,i,player3cards,"player4","player2")  
+                });
+            }
+    
+            break;
+        case "player4":
+            jelenlegi.innerHTML = "Jelenlegi játékos: Player 4";
+            container.innerHTML = '';
+            for (let i = 0; i < player4cards.length; i++) {
+                const img=document.createElement("img");
+                img.src=`cards/${player4cards[i]}.png`;
+                img.classList.add("card")
+                container.appendChild(img)
+                img.addEventListener('click', (event) => {
+                    mindenegben(event,i,player4cards,"player1","player3")
+                });
+            }
+    
+            break;
+        }
+    }
+    closePopup();
+    filltombs(); 
+    starter();
+    showcard();
+    
+    console.log(player1cards)
+    console.log(player2cards)
+    console.log(player3cards)
+    console.log(player4cards)
+    
+
+
+}
